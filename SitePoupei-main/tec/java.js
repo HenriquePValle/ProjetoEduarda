29e209a71389c4ff1dcdcdd3628affbad3bcd427
dudazap <125655570+dudazap@users.noreply.github.com>
2023-09-27 12:04:34 -0300

    Add files via upload

---
+++ SitePoupei-main/tec/java.js
@@ -0,0 +1,20 @@
+var itens = [
+    "bala fini",
+    "salgadinho", 
+    "doritos"
+];
+
+
+
+function pesquisar(){
+    console.log("tá funcionado")
+}
+
+onkeydown = ()=>{
+    pesquisar();
+};
+
+document.querySelector(".ladinho").onclick = function(){
+    document.querySelector("#anunc").scrollBy(365,0);
+};
+
